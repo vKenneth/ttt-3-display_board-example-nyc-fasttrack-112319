0b738070c9f9dd13b8c8c9664064ee90236a9e0b
K e n V <kenneth_valdivia@yahoo.com>
2019-11-24 08:09:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+// ♥ irb
+001:0 > def display_board
+002:1 >     puts "A Tic Tac Toe Board"
+003:1 >   end
+=> :display_board
+004:0 > display_board
+A Tic Tac Toe Board
+=> nil
+
